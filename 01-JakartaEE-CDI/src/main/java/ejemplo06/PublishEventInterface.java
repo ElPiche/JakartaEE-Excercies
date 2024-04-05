9203581123ba50285a65db0914ddb930c517f00e
ElPiche <lucasfidel85@gmail.com>
2024-04-05 20:29:42 -0300

    Elimino archivos para subir otros

--- 01-JakartaEE-CDI/src/main/java/ejemplo06/PublishEventInterface.java
+++
@@ -1,6 +0,0 @@
-package ejemplo06;
-
-public interface PublishEventInterface {
-	void publicarEvento(String descEvento);
-	void publishEvent(String eventDescritption);
-}
--- 01-JakartaEE-CDI/src/main/java/ejemplo07/dominio/AutoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package ejemplo07.dominio;
-
-import java.util.List;
-
-public interface AutoRepositorio {
-	
-	List<Auto> findAll();
-	void agregar(Auto  autinho);
-}
